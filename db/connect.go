0322c8e5ef9bf591db87ac01720106fb5d312daa
leeway <niv1589@knou.ac.kr>
2024-12-12 10:47:05 +0900

    feat(db): added connection pooling for PostgreSQL

--- db/connect.go
+++ db/connect.go
@@ -6,12 +6,32 @@ import (
 	"os"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func connectPG(url string, ctx *context.Context) *pgx.Conn {
+func connectPGPool(url string, ctx *context.Context) *pgxpool.Pool {
 
 	fmt.Println("trying to connect to db : ", url)
-	conn, err := pgx.Connect(*ctx, url)
+	config, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		panic(err)
+	}
+	pool, err := pgxpool.NewWithConfig(*ctx, config)
+	if err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	return pool
+
+}
+func connectPG(url string, ctx context.Context) *pgx.Conn {
+
+	fmt.Println("trying to connect to db : ", url)
+	conn, err := pgx.Connect(ctx, url)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
