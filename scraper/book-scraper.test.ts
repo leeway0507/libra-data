a4b2b511701996f04db966cd919b5fc4b3d4a9eb
leeway <niv1589@knou.ac.kr>
2024-12-13 17:34:15 +0900

    feat(scraper): enhance book scraper with data extraction and file handling improvements

--- scraper/book-scraper.test.ts
+++ scraper/book-scraper.test.ts
@@ -1,9 +1,13 @@
-import { kyoboScraper } from "./book-scraper";
+import { kyoboScraper, type ScrapData } from "./book-scraper";
+import { expectTypeOf } from 'expect-type';
+import path from 'path';
+import fsAsync from "fs/promises"
+import fsSync from "fs"
 
 describe(('Consortium List SubScraper '), () => {
     const scraper = new kyoboScraper();
-    const testIsbn = "9791163034735" // 점프 투 파이썬
-    scraper.isbn = testIsbn
+    scraper.dataPath = "/Users/yangwoolee/repo/libra-data/scraper/temp/test"
+    scraper.isbn = "9791163034735" // 점프 투 파이썬
 
     test('should loadSpecPage ', () => {
         scraper.loadSpecPage();
@@ -20,19 +24,43 @@ describe(('Consortium List SubScraper '), () => {
         expect(scraper.page.url).toEqual("")
     });
 
-    test('should saveBookHtml ', () => {
-        scraper.saveHtml();
-        expect(scraper.page.url).toEqual("")
+    test('should saveBookHtml ', async () => {
+        await scraper.initBrowser()
+        const specPath = "file:///Users/yangwoolee/repo/libra-data/scraper/temp/test/kyobo/spec.html"
+        await scraper.page.goto(specPath)
+
+        await scraper.saveHtml();
+
+        const localFilePath = path.join(scraper.dataPath, scraper.scraperName, scraper.isbn + ".html")
+        expect(fsSync.existsSync(localFilePath)).toBe(true)
+        if (fsSync.existsSync(localFilePath)) {
+            fsSync.rmSync(localFilePath)
+        }
     });
 
     test('should saveImage ', () => {
         expect(scraper.saveImage(Buffer.from("test"))).toEqual("ok")
     });
 
+    test('should extract bookinfo', async () => {
+        await scraper.initBrowser()
+        const specPath = "file:///Users/yangwoolee/repo/libra-data/scraper/temp/test/kyobo/spec.html"
+        await scraper.page.goto(specPath)
+
+        // console.log(await scraper.extractData())
+        expectTypeOf(await scraper.extractData()).toMatchTypeOf({
+            toc: "string",
+            recommendation: "string",
+            description: "string",
+            source: "string",
+            url: "string"
+        })
+    })
+
     test('should searchBook ', async () => {
         await scraper.initBrowser()
-        const testPath = "file:///Users/yangwoolee/repo/libra-data/scraper/temp/test/kyobo/search.html"
-        const url = await scraper.searchBook(testPath);
+        const searchPath = "file:///Users/yangwoolee/repo/libra-data/scraper/temp/test/kyobo/search.html"
+        const url = await scraper.searchBook(searchPath);
 
         expect(url).toEqual("https://product.kyobobook.co.kr/detail/S000202532365")
     });
