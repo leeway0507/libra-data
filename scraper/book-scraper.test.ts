1e4bac861d77f01c67b1b5c12592dfa8c08f2072
leeway <niv1589@knou.ac.kr>
2024-12-15 15:33:21 +0900

    chore: add Prettier

--- scraper/book-scraper.test.ts
+++ scraper/book-scraper.test.ts
@@ -1,95 +1,126 @@
-import { kyoboScraper, initBrowser, scrapIsbns } from "./book-scraper";
-import { expectTypeOf } from 'expect-type';
-import path from 'path';
-import { describe, test as it, expect } from "bun:test";
+import {
+    kyoboScraper,
+    initBrowser,
+    scrapIsbns,
+    updateTargetResult,
+} from "./book-scraper"
+import { expectTypeOf } from "expect-type"
+import path from "path"
+import { describe, test as it, expect } from "bun:test"
 import fsSync from "fs"
 
-describe("multi scrap", () => {
-    it('should execute multi page ', async () => {
-        await scrapIsbns(["9791163034735", "9791163034735"], 2)
-    }, { timeout: 10_000 });
+describe("multi page scrap", () => {
+    it(
+        "should execute multi page ",
+        async () => {
+            await scrapIsbns(["9791163034735", "9791163034735"], 2)
+        },
+        { timeout: 10_000 }
+    )
 
+    it("should update target ", async () => {
+        const target = ["1234", "5678"]
+        const result = ["1234"]
+        await updateTargetResult(target, result)
+    })
 })
 
-describe(('book scraper'), async () => {
+describe("book scraper", async () => {
     const browser = await initBrowser()
 
-    const scraper = new kyoboScraper(await browser.newPage()); // 점프 투 파이썬
-    scraper.dataPath = "/Users/yangwoolee/repo/libra-data/scraper/temp/test"
+    const scraper = new kyoboScraper(await browser.newPage()) // 점프 투 파이썬
+    scraper.dataPath = `${__dirname}/temp/test`
     scraper.isbn = "9791163034735"
 
-    const searchPath = "file:///Users/yangwoolee/repo/libra-data/scraper/temp/test/kyobo/search.html"
-    const specPath = `file:///Users/yangwoolee/repo/libra-data/scraper/temp/test/kyobo/${scraper.isbn}.html`
+    const searchPath = `file://${__dirname}/temp/test/kyobo/search.html`
+    const specPath = `file://${__dirname}/temp/test/kyobo/${scraper.isbn}.html`
 
-    it('should loadWebSpecPage ', async () => {
-        const isloaded = await scraper.loadWebSpecPage();
-        expect(isloaded).toBe(true)
-    }, { timeout: 10_000 });
+    it(
+        "should loadWebSpecPage ",
+        async () => {
+            const isloaded = await scraper.loadWebSpecPage()
+            expect(isloaded).toBe(true)
+        },
+        { timeout: 10_000 }
+    )
 
+    it(
+        "should loadSpecPage ",
+        async () => {
+            const isloaded = await scraper.loadSpecPage()
+            expect(isloaded).toBe(true)
+        },
+        { timeout: 10_000 }
+    )
 
-    it('should loadSpecPage ', async () => {
-        const isloaded = await scraper.loadSpecPage();
-        expect(isloaded).toBe(true)
-    }, { timeout: 10_000 });
-
-    it('should saveBookHtml ', async () => {
+    it("should saveBookHtml ", async () => {
         await scraper.page.goto(specPath)
-        await scraper.saveHtml();
+        await scraper.saveHtml()
 
-        const localFilePath = path.join(scraper.dataPath, scraper.scraperName, scraper.isbn + ".html")
+        const localFilePath = path.join(
+            scraper.dataPath,
+            scraper.scraperName,
+            scraper.isbn + ".html"
+        )
         expect(fsSync.existsSync(localFilePath)).toBe(true)
         // if (fsSync.existsSync(localFilePath)) {
         //     fsSync.rmSync(localFilePath)
         // }
-    });
-    it('should loadLocalSpecPage ', async () => {
-        const isloaded = await scraper.loadLocalSpecPage();
+    })
+    it("should loadLocalSpecPage ", async () => {
+        const isloaded = await scraper.loadLocalSpecPage()
         expect(isloaded).toBe(true)
-    });
-    it('should get image url', async () => {
+    })
+    it("should get image url", async () => {
         await scraper.page.goto(specPath)
         await scraper.extractImageSrc()
     })
 
-    it('should saveImage ', async () => {
+    it("should saveImage ", async () => {
         await scraper.page.goto(specPath)
 
         expect(await scraper.saveImage()).toBe(true)
-    });
+    })
 
-    it('should extract bookinfo', async () => {
+    it("should extract bookinfo", async () => {
         await scraper.page.goto(specPath)
 
         // console.log(await scraper.extractData())
         expectTypeOf(await scraper.extractData()).toMatchTypeOf({
+            isbn: "string",
             toc: "string",
             recommendation: "string",
             description: "string",
             source: "string",
-            url: "string"
+            url: "string",
         })
     })
 
-    it('should searchBook ', async () => {
-        const url = await scraper.searchBook(searchPath);
-        expect(url).toEqual("https://product.kyobobook.co.kr/detail/S000202532365")
-    });
+    it("should searchBook ", async () => {
+        const url = await scraper.searchBook(searchPath)
+        expect(url).toEqual(
+            "https://product.kyobobook.co.kr/detail/S000202532365"
+        )
+    })
 
-    it('should get Recommendation ', async () => {
+    it("should get Recommendation ", async () => {
         await scraper.page.goto(specPath)
         expect((await scraper.getRecommendation()).length).toBeGreaterThan(0)
-        console.log("getRecommendation", (await scraper.getRecommendation()).length)
-    });
+        console.log(
+            "getRecommendation",
+            (await scraper.getRecommendation()).length
+        )
+    })
 
-    it('should get Toc ', async () => {
+    it("should get Toc ", async () => {
         await scraper.page.goto(specPath)
         expect((await scraper.getToc()).length).toBeGreaterThan(0)
         console.log("getToc", (await scraper.getToc()).length)
-    });
+    })
 
-    it('should get Description ', async () => {
+    it("should get Description ", async () => {
         await scraper.page.goto(specPath)
         expect((await scraper.getDescription()).length).toBeGreaterThan(0)
         console.log("getDescription", (await scraper.getDescription()).length)
-    });
+    })
 })
--- scraper/book-scraper.ts
+++ scraper/book-scraper.ts
@@ -1,14 +1,118 @@
-import { chromium, type Page, type Browser, BrowserContext } from 'playwright';
-import path from 'path';
-import { fetch } from 'bun';
+import { chromium, type Page, type Browser, BrowserContext } from "playwright"
+import path from "path"
+import { fetch } from "bun"
 import fsAsync from "fs/promises"
 import pino, { type Logger } from "pino"
 import pretty from "pino-pretty"
+import { format } from "date-fns"
+import * as csv from "fast-csv"
 
-const CURRENT_PATH = "/Users/yangwoolee/repo/libra-data/scraper"
-const SEARCH_URL = "https://search.kyobobook.co.kr/search?gbCode=TOT&target=total"
+const SEARCH_URL =
+    "https://search.kyobobook.co.kr/search?gbCode=TOT&target=total"
+
+export async function initBrowser(
+    headless: boolean = false
+): Promise<BrowserContext> {
+    const bravePath =
+        "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+    const browser = await chromium.launch({
+        executablePath: bravePath,
+        headless,
+    })
+    return await browser.newContext()
+}
+
+export async function loadTargets(): Promise<string[]> {
+    const targetFile = Bun.file(
+        path.join(__dirname, "data/target", "target.csv")
+    )
+    const targetText = await targetFile.text()
+    var csvArr = targetText.split("\n")
+
+    if (csvArr[0] !== "isbn,status") {
+        const newCsvArr = addStatusColumn(csvArr)
+        await Bun.write(targetFile, newCsvArr.join("\n"))
+        csvArr = newCsvArr
+    }
+    csvArr.shift() // drop header
+    return csvArr.reduce((acc, row) => {
+        const cols = row.split(",")
+        if (cols[1] === "N") {
+            acc.push(cols[0])
+        }
+        return acc
+    }, [] as string[])
+}
+
+function addStatusColumn(csvArr: string[]): string[] {
+    csvArr.shift() // drop header
+    return [
+        "isbn,status",
+        ...csvArr.map((row) => [row.replace(/^\"|\"$/g, ""), "N"].join(",")),
+    ]
+}
+
+export async function scrapIsbns(
+    isbns: string[],
+    numWorker: number
+): Promise<ScrapData[]> {
+    const ctx = await initBrowser()
+    const chunck = Math.ceil(isbns.length / numWorker)
+    const isbnsChunk = Array(numWorker)
+        .fill("")
+        .map((_, idx) => isbns.slice(idx * chunck, (idx + 1) * chunck))
+        .filter((item) => item.length > 0)
+
+    const _numWorker = Math.min(isbnsChunk.length, numWorker)
+    const workers = await Promise.all(
+        Array(_numWorker)
+            .fill(null)
+            .map(async () => new kyoboScraper(await ctx.newPage()))
+    )
+
+    const result = await Promise.all(
+        workers.map((worker, idx) => worker.execAll(isbnsChunk[idx]))
+    )
+    return result.flat(1).filter((x) => x != null)
+}
+
+export async function saveScrapResult(data: ScrapData[]): Promise<string[]> {
+    const fileName = format(Date.now(), "yyyyMMdd-HHmmss")
+    const filePath = path.join(__dirname, "data/kyobo", fileName + ".json")
+    await Bun.write(filePath, JSON.stringify(data), { createPath: true })
+    return data.map((d) => d.isbn)
+}
+
+export async function updateTargetResult(
+    targetArr: string[],
+    resultArr: string[]
+) {
+    const targetFile = Bun.file(
+        path.join(__dirname, "data/target", "target.csv")
+    )
+    const targetText = await targetFile.text()
+    var csvArr = targetText.split("\n")
+
+    const resultIsbnSet = new Set(resultArr)
+    const notFoundSet = new Set(targetArr.filter((t) => !resultIsbnSet.has(t)))
+
+    const newCsvArr = csvArr.map((rowStr) => {
+        const row = rowStr.split(",")
+        if (resultIsbnSet.has(row[0])) {
+            row.pop()
+            row.push("Y")
+        } else if (notFoundSet.has(row[0])) {
+            row.pop()
+            row.push("notFound")
+        }
+        return row.join(",")
+    })
+
+    await Bun.write(targetFile, newCsvArr.join("\n"))
+}
 
 export type ScrapData = {
+    isbn: string
     toc: string
     recommendation: string
     description: string
@@ -17,8 +121,8 @@ export type ScrapData = {
 }
 
 const LoggingFile = pino.destination({
-    dest: path.join(CURRENT_PATH, "scraplogger.log"),
-    append: 'stack'
+    dest: path.join(__dirname, "scraplogger.log"),
+    append: "stack",
 })
 
 const LoggerInstance: Logger = pino(
@@ -27,42 +131,10 @@ const LoggerInstance: Logger = pino(
         level: "debug",
     },
     pino.multistream([
-        { level: 'info', stream: pretty() },
-        { level: 'debug', stream: LoggingFile },
-    ]),
+        { level: "info", stream: pretty() },
+        { level: "debug", stream: LoggingFile },
+    ])
 )
-
-export async function initBrowser(headless: boolean = false): Promise<BrowserContext> {
-    const bravePath = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser';
-    const browser = await chromium.launch({
-        executablePath: bravePath,
-        headless,
-    });
-    return await browser.newContext()
-}
-
-export async function scrapIsbns(isbns: string[], numWorker: number): Promise<ScrapData[]> {
-    const ctx = await initBrowser()
-
-    const isbnsChunk: string[][] = [];
-    while (isbns.length > 0)
-        isbnsChunk.push(isbns.splice(0, numWorker));
-
-    const _numWorker = Math.min(isbnsChunk.length, numWorker)
-    const workers = await Promise.all(
-        Array(_numWorker)
-            .fill(null)
-            .map(async () => new kyoboScraper(await ctx.newPage()))
-    );
-
-    const result = await Promise.all(
-        workers.map((worker, idx) =>
-            worker.execAll(isbnsChunk[idx]))
-    )
-    return result.flat(1).filter(x => x != null)
-}
-
-
 
 interface BookScraper {
     exec(): Promise<ScrapData | null>
@@ -76,7 +148,7 @@ interface BookScraper {
 }
 
 export class kyoboScraper implements BookScraper {
-    page!: Page;
+    page!: Page
     isbn!: string
     scraperName = "kyobo"
     dataPath = "/Users/yangwoolee/repo/libra-data/scraper/temp/html"
@@ -90,7 +162,7 @@ export class kyoboScraper implements BookScraper {
         if (!isbns) return []
         const result: (ScrapData | null)[] = []
         for (const isbn of isbns) {
-            this.isbn = isbn;
+            this.isbn = isbn
             result.push(await this.exec())
         }
         return result
@@ -107,8 +179,6 @@ export class kyoboScraper implements BookScraper {
         return await this.extractData()
     }
 
-
-
     async loadSpecPage() {
         const isLocalLoaded = await this.loadLocalSpecPage()
         if (isLocalLoaded) {
@@ -122,19 +192,22 @@ export class kyoboScraper implements BookScraper {
             return true
         }
         return false
-
     }
     async loadLocalSpecPage(): Promise<boolean> {
-        const localFilePath = path.join(this.dataPath, this.scraperName, this.isbn + ".html")
+        const localFilePath = path.join(
+            this.dataPath,
+            this.scraperName,
+            this.isbn + ".html"
+        )
         if (await Bun.file(localFilePath).exists()) {
-            this.logger.debug({ localFilePath: path.join("file://", localFilePath) })
+            this.logger.debug({
+                localFilePath: path.join("file://", localFilePath),
+            })
             await this.page.goto(path.join("file://", localFilePath))
             return true
         }
         this.logger.debug("localPath : not exist")
         return false
-
-
     }
     async loadWebSpecPage(): Promise<boolean> {
         const searchURL = new URL(SEARCH_URL)
@@ -145,7 +218,6 @@ export class kyoboScraper implements BookScraper {
         if (specUrl === "") return false
         await this.page.goto(specUrl)
         return true
-
     }
     async saveHtml() {
         if (this.page.url().startsWith("file://")) return
@@ -160,19 +232,18 @@ export class kyoboScraper implements BookScraper {
         const isFileExist = await Bun.file(localFilePath).exists()
         this.logger.debug({ isFileExist }, "saveHtml")
 
-
         if (!isFileExist) {
             const html = await this.page.content()
             return await Bun.write(Bun.file(localFilePath), html)
         }
-
     }
     async extractImageSrc(): Promise<string> {
         const imgXpath = '//div[contains(@class, "portrait_img_box")]/img'
         const loc = this.page.locator(imgXpath)
-        const src = await loc.count() && await loc.first().getAttribute("src") || ""
-        this.logger.debug({ src }, "Extracted image source");
-
+        const src =
+            ((await loc.count()) && (await loc.first().getAttribute("src"))) ||
+            ""
+        this.logger.debug({ src }, "Extracted image source")
 
         return src
     }
@@ -180,21 +251,24 @@ export class kyoboScraper implements BookScraper {
         const src = await this.extractImageSrc()
 
         if (src) {
-            const response = await fetch(src);
-            const arrayBuffer = await response.arrayBuffer();
+            const response = await fetch(src)
+            const arrayBuffer = await response.arrayBuffer()
 
             const bookNameXpath = "//span[@class='prod_title']"
-            const bookName = await this.page.locator(bookNameXpath).first().textContent()
+            const bookName = await this.page
+                .locator(bookNameXpath)
+                .first()
+                .textContent()
             const exName = path.extname(src)
 
             const imagePath = path.join(
                 this.dataPath,
                 this.scraperName,
                 "image",
-                `${this.isbn}-${bookName}${exName}`
+                `${this.isbn}-${bookName?.replace("/", "_")}${exName}`
             )
             this.logger.debug({ imagePath }, "saveImage")
-            await Bun.write(imagePath, arrayBuffer);
+            await Bun.write(imagePath, arrayBuffer)
             return true
         }
         return false
@@ -203,56 +277,65 @@ export class kyoboScraper implements BookScraper {
         await this.page.goto(searchURL)
         await this.page.waitForLoadState("domcontentloaded")
 
-        const selector = '//ul[@class="prod_list"]//a[@class="prod_link"]';
-        const loc = this.page.locator(selector);
+        const selector = '//ul[@class="prod_list"]//a[@class="prod_link"]'
+        const loc = this.page.locator(selector)
         this.logger.debug("possible books lengths : %d", await loc.count())
-        const specUrl = await loc.count() > 0 ? await loc.first().getAttribute("href") : ""
+        const specUrl =
+            (await loc.count()) > 0
+                ? await loc.first().getAttribute("href")
+                : ""
         this.logger.debug("selected books url : %s ", specUrl)
         return specUrl || ""
     }
     async extractData(): Promise<ScrapData> {
         const urlXpath = "meta[property='og:url']"
-        const url = await this.page.locator(urlXpath).first().getAttribute("content")
+        const url = await this.page
+            .locator(urlXpath)
+            .first()
+            .getAttribute("content")
         const toc = await this.getToc()
         const recommendation = await this.getRecommendation()
         const description = await this.getDescription()
 
-        this.logger.debug({
-            toc: toc.length,
-            recommendation: recommendation.length,
-            description: description.length,
-            source: this.scraperName,
-            url
-        }, "extractData")
+        this.logger.debug(
+            {
+                isbn: this.isbn,
+                toc: toc.length,
+                recommendation: recommendation.length,
+                description: description.length,
+                source: this.scraperName,
+                url,
+            },
+            "extractData"
+        )
         return {
+            isbn: this.isbn,
             toc,
             recommendation,
             description,
             source: this.scraperName,
-            url: url || ""
+            url: url || "",
         }
-
     }
     async getRecommendation(): Promise<string> {
-        const recoXpathFirst = '//div[@class="product_detail_area book_publish_review"]'
+        const recoXpathFirst =
+            '//div[@class="product_detail_area book_publish_review"]'
         const loc = this.page.locator(recoXpathFirst)
         const recoXpathSec = '//p[@class="info_text"]'
         var loc2
-        if (await loc.count() > 0) {
+        if ((await loc.count()) > 0) {
             loc2 = loc.locator(recoXpathSec)
         }
-        return await loc2?.count() && await loc2?.textContent() || ""
+        return ((await loc2?.count()) && (await loc2?.innerText())) || ""
     }
     async getToc(): Promise<string> {
         const tocXpath = '//li[@class="book_contents_item"]'
         const loc = this.page.locator(tocXpath)
-        return await loc.count() && await loc.textContent() || ""
+        return ((await loc.count()) && (await loc.innerText())) || ""
     }
     async getDescription(): Promise<string> {
         const descXpath = '//div[@class="intro_bottom"]'
         const loc = this.page.locator(descXpath)
-        return await loc.count() && await loc.textContent() || ""
+        return ((await loc.count()) && (await loc.innerText())) || ""
     }
-
 }
-
