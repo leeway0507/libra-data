043a4acb58a931f17d11444a641453ade5447dd9
leeway <niv1589@knou.ac.kr>
2024-12-15 15:33:21 +0900

    chore: add Prettier

--- scraper/index.ts
+++ scraper/index.ts
@@ -1,21 +1,23 @@
-import { scrapIsbns } from "./book-scraper"
+import {
+    scrapIsbns,
+    saveScrapResult,
+    loadTargets,
+    updateTargetResult,
+} from "./book-scraper"
 
 async function main() {
-    const isbnArr = [
-        "9791156000846",
-        "9791193217078",
-        "9791197932564",
-        "9788980783144",
-        "9791192987354",
-        "9788980783144",
-        "9791158394622",
-    ]
-    const x = await scrapIsbns(isbnArr, 3)
-    console.log(x)
+    const targetIsbns = await loadTargets().then((a) => a.slice(0, 40))
+    const scrapResult = await scrapIsbns(targetIsbns, 4)
+    const resultIsbns = await saveScrapResult(scrapResult)
+
+    console.log("scrap Length :", resultIsbns.length)
+    await updateTargetResult(targetIsbns, resultIsbns)
 }
 
-main().catch((err) => {
-    console.error("Error occurred:", err)
-}).finally(() => {
-    process.exit(0) // 프로세스 강제 종료
-})
+main()
+    .catch((err) => {
+        console.error("Error occurred:", err)
+    })
+    .finally(() => {
+        process.exit(0) // 프로세스 강제 종료
+    })
