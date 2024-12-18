ebaf2796fa8201a9dc0c27333066121edbc4c68f
leeway <niv1589@knou.ac.kr>
2024-12-19 08:47:32 +0900

    feat(scraper): add library scraper  for download libData directly

---
+++ scraper/library-scraper.test.ts
@@ -0,0 +1,114 @@
+import { LibScraper } from "./library-scraper"
+import { initBrowser } from "./book-scraper"
+import { describe, test as it, expect } from "bun:test"
+import fs from "fs"
+import path from "path"
+
+process.env.DATA_PATH = "/Users/yangwoolee/repo/libra-data/data/test"
+
+describe("library scraper", async () => {
+    const ctx = await initBrowser()
+    const libScraperInstance = new LibScraper(ctx)
+    const newPage = await ctx.newPage()
+    it(
+        "should select location ",
+        async () => {
+            const isSelected = await libScraperInstance.selectLocation(newPage)
+            expect(isSelected).toBe(true)
+        },
+        { timeout: 10_000 }
+    )
+    it(
+        "should select LibType ",
+        async () => {
+            await newPage.goto("https://www.data4library.kr/openDataL")
+            await newPage.waitForLoadState("domcontentloaded")
+            const isSelected = await libScraperInstance.selectLibType(newPage)
+            expect(isSelected).toBe(true)
+        },
+        { timeout: 10_000 }
+    )
+    it("should test rest", () => {
+        Array(10)
+            .fill("")
+            .map((k, v) => {
+                const value = v + 1
+                console.log(value, (value % 5) - 1)
+            })
+    })
+    it(
+        "should move to target page",
+        async () => {
+            const targetPage = 2
+            await newPage.goto("https://www.data4library.kr/openDataL")
+            await newPage.waitForLoadState("domcontentloaded")
+
+            await libScraperInstance.moveToTargetPage(newPage, targetPage)
+
+            const pageNationXPath = "//a[@class='page']"
+            const loc = newPage.locator(pageNationXPath)
+            const title = await loc.nth(targetPage - 1).getAttribute("title")
+            expect(title).toBe("현재 페이지")
+        },
+        { timeout: 10_000 }
+    )
+    it(
+        "should get lib data and filter if files exist",
+        async () => {
+            await newPage.goto("https://www.data4library.kr/openDataL")
+            await newPage.waitForLoadState("networkidle")
+            const libArr = await libScraperInstance.getLibList(newPage)
+            console.log(libArr)
+        },
+        { timeout: 10_000 }
+    )
+    it(
+        "should filter if files exist",
+        async () => {
+            const libArrMock = [
+                {
+                    libName: "서울특별시교육청양천도서관",
+                    uploadDate: "2024-12-01",
+                    nth: 0,
+                },
+                {
+                    libName: "KB국민은행과 함께하는 나무 작은도서관",
+                    uploadDate: "2024-12-01",
+                    nth: 1,
+                },
+            ]
+            const libFilteredMock = libScraperInstance.exctractCandidate(libArrMock)
+            expect(libFilteredMock.length).toBe(1)
+
+            await libScraperInstance.moveToDownloadPage(newPage, libArrMock[0])
+            expect(newPage.url()).toBe("https://www.data4library.kr/openDataV")
+            newPage.waitForTimeout(1000)
+        },
+        { timeout: 10_000 }
+    )
+    it(
+        "should move to download page",
+        async () => {
+            await newPage.goto("https://www.data4library.kr/openDataL")
+            await newPage.waitForLoadState("domcontentloaded")
+
+            const libArrMock = {
+                libName: "test_library",
+                uploadDate: "2024-12-01",
+                nth: 0,
+            }
+
+            await libScraperInstance.moveToDownloadPage(newPage, libArrMock)
+            expect(newPage.url()).toBe("https://www.data4library.kr/openDataV")
+            newPage.waitForTimeout(1000)
+
+            await libScraperInstance.downloadLibData(newPage, libArrMock)
+
+            const isFileExist = fs.existsSync(
+                path.join(process.env.DATA_PATH!, "library", libArrMock.libName, libArrMock.uploadDate + ".xlsx")
+            )
+            expect(isFileExist).toBe(true)
+        },
+        { timeout: 30_000 }
+    )
+})
---
+++ scraper/library-scraper.ts
@@ -0,0 +1,117 @@
+import { BrowserContext, Page } from "playwright"
+import path from "path"
+import fsSync from "fs"
+import fsAsync from "fs/promises"
+
+if (!process.env.DATA_PATH) {
+    process.env.DATA_PATH = "/Users/yangwoolee/repo/libra-data/data"
+}
+
+type LibScrap = {
+    libName: string
+    uploadDate: string
+    nth: number
+}
+
+export class LibScraper {
+    ctx!: BrowserContext
+
+    constructor(page: BrowserContext) {
+        this.ctx = page
+    }
+
+    // async exec() {
+    //     const page = await this.ctx.newPage()
+    // }
+
+    async getDataByPagination(page: Page, targetPage: number) {
+        await this.selectLocation(page)
+        await this.selectLibType(page)
+        await this.moveToTargetPage(page, targetPage)
+        const libList: LibScrap[] = await this.getLibList(page)
+        const libFilteredList: LibScrap[] = this.exctractCandidate(libList)
+
+        if (libFilteredList.length > 0) {
+            for (const libScrap of libFilteredList) {
+                await this.moveToDownloadPage(page, libScrap)
+                await this.downloadLibData(page, libScrap)
+                await page.goBack()
+                await page.waitForLoadState("networkidle")
+            }
+        }
+    }
+
+    async selectLocation(page: Page): Promise<boolean> {
+        await page.goto("https://www.data4library.kr/openDataL")
+        await page.waitForLoadState("domcontentloaded")
+        const x = await page.selectOption("#p_region", "서울")
+        await page.waitForLoadState("networkidle")
+        return x.length > 0 && x[0] === "11"
+    }
+
+    async selectLibType(page: Page): Promise<boolean> {
+        const x = await page.selectOption("#libType", "공공")
+        await page.waitForLoadState("networkidle")
+        return x.length > 0
+    }
+
+    async moveToTargetPage(page: Page, targetPage: number) {
+        const pageNationXPath = "//a[@class='page']"
+        const loc = page.locator(pageNationXPath)
+        await loc.nth(targetPage - 1).click()
+        await page.waitForLoadState("networkidle")
+    }
+
+    async getLibList(page: Page): Promise<LibScrap[]> {
+        const tableXpath = "//tbody/tr"
+        const libNameXpath = "//td[@class='link_td']/a"
+        const uploadDateXpath = "//td[@class='br_none']"
+        const libList = await page.locator(tableXpath).all()
+        return Promise.all(
+            libList.map(async (v, idx) => {
+                const lib1 = v.locator(libNameXpath)
+                const lib2 = v.locator(uploadDateXpath)
+                const libName = (await lib1.count()) > 0 && (await lib1.innerText())
+                const uploadDate = (await lib2.count()) > 0 && (await lib2.innerText())
+                return {
+                    libName: libName ? libName.replace("장서/대출 데이터", "").trim() : "",
+                    uploadDate: uploadDate || "",
+                    nth: idx,
+                }
+            })
+        )
+    }
+    exctractCandidate(libList: LibScrap[]): LibScrap[] {
+        const isCandidate = (l: LibScrap) => {
+            const folderPath = path.join(process.env.DATA_PATH!, "library", l.libName)
+            const filePath = path.join(folderPath, l.uploadDate + ".xlsx")
+            return !fsSync.existsSync(filePath)
+        }
+
+        return libList.filter(isCandidate)
+    }
+    async moveToDownloadPage(page: Page, libData: LibScrap) {
+        const libNameXpath = "//td[@class='link_td']/a"
+        await page.locator(libNameXpath).nth(libData.nth).click()
+        await page.waitForLoadState("networkidle")
+    }
+    async downloadLibData(page: Page, libData: LibScrap) {
+        const dateXpath = "//section/div[2]/div[3]/table/tbody/tr[1]/td[3]"
+        const loc = page.locator(dateXpath)
+        if ((await loc.innerText()) !== libData.uploadDate) {
+            console.log("not match", libData)
+            return
+        }
+
+        const downloadXpath = "//section/div[2]/div[3]/table/tbody/tr[1]/td[4]/a[2]"
+        const downloadLoc = page.locator(downloadXpath)
+
+        await fsAsync.mkdir(process.env.DATA_PATH!, { recursive: true })
+
+        const downloadPromise = page.waitForEvent("download")
+        await downloadLoc.click()
+        const download = await downloadPromise
+        const folderPath = path.join(process.env.DATA_PATH!, "library", libData.libName)
+        await download.saveAs(path.join(folderPath, libData.uploadDate + ".xlsx"))
+    }
+}
